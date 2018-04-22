25b0794093056b5a96060edd2bcd260162ba3d13
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-22 17:46:28 -0500

    calling weather api

--- app.js
+++ app.js
@@ -15,11 +15,22 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if(errorMessage) {
-        console.log(console.errorMessage);
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if(errorMessage) {
+//         console.log(console.errorMessage);
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+
+request({
+    url: `https://api.darksky.net/forecast/03516b4d2a76bef2a40e0bd3f36b89c2/35.4791403,-97.23171400000001`,
+    json: true
+}, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        console.log(body.currently.temperature);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
-    }
+        console.log('Unable to fetch weather.');
+    } 
 });
 
