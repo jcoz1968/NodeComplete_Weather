2535b976ee65ee08362e18b16bf351c1fd8b0baf
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-22 18:13:25 -0500

    chaining callbacks

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
 const argv = yargs
     .options({
@@ -15,22 +16,21 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//     if(errorMessage) {
-//         console.log(console.errorMessage);
-//     } else {
-//         console.log(JSON.stringify(results, undefined, 2));
-//     }
-// });
-
-request({
-    url: `https://api.darksky.net/forecast/03516b4d2a76bef2a40e0bd3f36b89c2/35.4791403,-97.23171400000001`,
-    json: true
-}, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        console.log(body.currently.temperature);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if(errorMessage) {
+        console.log(errorMessage);
     } else {
-        console.log('Unable to fetch weather.');
-    } 
+        console.log(results.address);
+        // console.log(JSON.stringify(results, undefined, 2));
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+            if (errorMessage) {
+                console.log(errorMessage);
+            } else {
+                console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}`);
+            }
+        });        
+    }
 });
+
+
 
